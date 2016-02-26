3abb5d2c64e376f1edcc115172e8a0a94b3ba71a
zmsch27 <zmsch27@sina.com>
2016-02-26 16:31:04 +0800

    modify file at company

--- Python_Function.py
+++ Python_Function.py
@@ -118,4 +118,80 @@ print('add_end_None默认参数：', add_end_None())
 #此外，由于对象不变，多任务环境下同时读取对象不需要加锁，同时读一点问题都没有。我们在编写程序时，如果可以设计一个不变对象，那就尽量设计成不变对象。
 
 #可变参数-----------------
+#在Python函数中，还可以定义可变参数。顾名思义，可变参数就是传入的参数个数是可变的，可以是1个、2个到任意个，还可以是0个
+print('-----------------------------------------')
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n * n
+    return sum
+#调用该函数时，可以传入任意个参数，包括0个参数
+print('calc(1, 2) =', calc(1, 2))
+print('calc() =', calc())
+#如果已经有一个list或者tuple，要调用一个可变参数:
+nums = [1, 2, 3]
+print('calc(nums[0], nums[1], nums[2]) =', calc(nums[0], nums[1], nums[2]))
+#Python允许你在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去：
+#*nums表示把nums这个list的所有元素作为可变参数传进去
+nums = [1, 2, 3]
+print('calc(*nums) =', calc(*nums))
 
+#关键字参数---------------------
+#关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict
+print('-----------------------------------------')
+def person(name, age, **kw):
+	print('name:', name, 'age:', age, 'other:', kw)
+#函数person除了必选参数name和age外，还接受关键字参数kw。在调用该函数时，可以只传入必选参数
+person('ch', 25)
+#也可以传入任意个数的关键字参数
+person('Bob', 35, city='Beijing')
+person('Adam', 45, gender='M', job='Engineer')
+#和可变参数类似，也可以先组装出一个dict，然后，把该dict转换为关键字参数传进去
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, city=extra['city'], job=extra['job'])
+#当然，上面复杂的调用可以用简化的写法
+#**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，
+#注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra
+person('Jack', 24, **extra)
+
+#命名关键字参数--------------------
+#如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下:
+print('-----------------------------------------')
+def person(name, age, *, city, job):
+	print(name, age, city, job)
+#和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+#命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错
+person('Jack', 24, city='Beijing', job='Engineer')
+#命名关键字参数可以有缺省值，从而简化调用
+def person(name, age, *, city = 'BeiJing', job):
+	print(name, age, city, job)
+#由于命名关键字参数city具有默认值，调用时，可不传入city参数
+person('Jack', 24, job='Engineer')
+#使用命名关键字参数时，要特别注意，*不是参数，而是特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数
+
+#参数组合-----------------------
+#在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用，除了可变参数无法和命名关键字参数混合。
+#但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数/命名关键字参数和关键字参数。!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+print('-----------------------------------------')
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+
+args = (1, 2, 3, 4)
+kw = {'d': 99, 'x': '#'}
+f1(*args, **kw)
+f2(1, 2, d=99, city='aa')
+args = (1, 2, 3)
+kw = {'d': 88, 'x': '#'}
+f2(*args, **kw)
+print('-----------------------------------------\n')
+
+#递归函数/////////////////////////////////////////////////////////
+#计算fact(n) = n! = 1 x 2 x 3 x ... x (n-1) x n = (n-1)! x n
+def fact(n):
+    if n==1:
+        return 1
+    return n * fact(n - 1)
+print('10! =', fact(10))
