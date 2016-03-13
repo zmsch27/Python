6e33ceb293be397314e4d49443dce9de76470502
zmsch27 <zmsch27@sina.com>
2016-03-13 15:33:12 +0800

    modify file at home

--- Python_OOP.py
+++ Python_OOP.py
@@ -77,3 +77,50 @@ print('-----------------------------------------\n')
 
 
 #访问限制/////////////////////////////////////
+#如果要让内部属性不被外部访问，可以把属性的名称前加上两个下划线__
+#在Python中，实例的变量名如果以__开头，就变成了一个私有变量（private），只有内部可以访问，外部不能访问
+class Student(object):
+	def __init__(self, name, score):
+		self.__name = name
+		self.__score = score
+	def print_score(self):
+		print('%s: %s' % (self.__name, self.__score))
+#改完后，对于外部代码来说，没什么变动，但是已经无法从外部访问实例变量.__name和实例变量.__score
+bart = Student('Bart', 89)
+bart.print_score()
+#这样就确保了外部代码不能随意修改对象内部的状态，这样通过访问限制的保护，代码更加健壮。
+#但是如果外部代码要获取name和score怎么办？可以给Student类增加get_name和get_score这样的方法
+#如果又要允许外部代码修改score怎么办？可以再给Student类增加set_score方法
+class Student(object):
+	def __init__(self, name, score):
+		self.__name = name
+		self.__score = score
+	def get_name(self):
+		return self.__name
+	def get_score(self):
+		return self.__score
+	def set_name(self, name):
+		self.__name = name
+	def set_score(self, score):
+		self.__score = score
+	def print_score(self):
+		print('%s: %s' % (self.__name, self.__score))
+bart = Student('Bart', 89)
+print('bart.get_name:', bart.get_name())
+print('bart.get_score:', bart.get_score())
+bart.set_name('dd')
+bart.set_score(999)
+print('bart.get_name:', bart.get_name())
+print('bart.get_score:', bart.get_score())
+#需要注意的是，在Python中，变量名类似__xxx__的，也就是以双下划线开头，并且以双下划线结尾的，是特殊变量
+#特殊变量是可以直接访问的，不是private变量，所以，不能用__name__、__score__这样的变量名
+#有些时候，你会看到以一个下划线开头的实例变量名，比如_name，这样的实例变量外部是可以访问的
+#但是，按照约定俗成的规定，当你看到这样的变量时，意思就是，“虽然我可以被访问，但是，请把我视为私有变量，不要随意访问”。
+#双下划线开头的实例变量是不是一定不能从外部访问呢？其实也不是。不能直接访问__name是因为Python解释器对外把__name变量改成了_Student__name
+#仍然可以通过_Student__name来访问__name变量：
+print('bart._Student__name =', bart._Student__name)
+#总的来说就是，Python本身没有任何机制阻止你干坏事，一切全靠自觉
+print('-----------------------------------------\n')
+
+
+#继承和多态////////////////////////////////////////
