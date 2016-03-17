98b98ade399d2b58d13e0e9fe24ba7b844e04ba2
zmsch27 <zmsch27@sina.com>
2016-03-17 16:32:35 +0800

    add and modify file at company

--- Python_HOOP.py
+++ Python_HOOP.py
@@ -75,7 +75,7 @@ class Student(object):
 s = Student()
 s.score = 60 # OK，实际转化为s.set_score(60)
 print('s.score =', s.score) # OK，实际转化为s.get_score()
-s.score = 9999
+#s.score = 9999
 #注意到这个神奇的@property，我们在对实例属性操作的时候，就知道该属性很可能不是直接暴露的，而是通过getter和setter方法来实现的。
 #还可以定义只读属性，只定义getter方法，不定义setter方法就是一个只读属性：
 class Student(object):
@@ -134,22 +134,214 @@ print('-----------------------------------------')
 #比如，让Ostrich除了继承自Bird外，再同时继承Runnable。这种设计通常称之为MixIn。
 #为了更好地看出继承关系，我们把Runnable和Flyable改为RunnableMixIn和FlyableMixIn。
 #类似的，你还可以定义出肉食动物CarnivorousMixIn和植食动物HerbivoresMixIn，让某个动物同时拥有好几个MixIn：
-class Dog(Mammal, RunnableMixIn, CarnivorousMixIn):
-    pass
-#MixIn的目的就是给一个类增加多个功能，这样，在设计类的时候，我们优先考虑通过多重继承来组合多个MixIn的功能，而不是设计多层次的复杂的继承关系。
-#Python自带的很多库也使用了MixIn。举个例子，Python自带了TCPServer和UDPServer这两类网络服务，而要同时服务多个用户就必须使用多进程或多线程模型
-#这两种模型由ForkingMixIn和ThreadingMixIn提供。通过组合，我们就可以创造出合适的服务来。
-#比如，编写一个多进程模式的TCP服务，定义如下：
-class MyTCPServer(TCPServer, ForkingMixIn):
-    pass
-#编写一个多线程模式的UDP服务，定义如下：
-class MyUDPServer(UDPServer, ThreadingMixIn):
-    pass
-#如果你打算搞一个更先进的协程模型，可以编写一个CoroutineMixIn：
-class MyTCPServer(TCPServer, CoroutineMixIn):
-    pass
+# class Dog(Mammal, RunnableMixIn, CarnivorousMixIn):
+#     pass
+# MixIn的目的就是给一个类增加多个功能，这样，在设计类的时候，我们优先考虑通过多重继承来组合多个MixIn的功能，而不是设计多层次的复杂的继承关系。
+# Python自带的很多库也使用了MixIn。举个例子，Python自带了TCPServer和UDPServer这两类网络服务，而要同时服务多个用户就必须使用多进程或多线程模型
+# 这两种模型由ForkingMixIn和ThreadingMixIn提供。通过组合，我们就可以创造出合适的服务来。
+# 比如，编写一个多进程模式的TCP服务，定义如下：
+# class MyTCPServer(TCPServer, ForkingMixIn):
+#     pass
+# #编写一个多线程模式的UDP服务，定义如下：
+# class MyUDPServer(UDPServer, ThreadingMixIn):
+#     pass
+# #如果你打算搞一个更先进的协程模型，可以编写一个CoroutineMixIn：
+# class MyTCPServer(TCPServer, CoroutineMixIn):
+#     pass
 #这样一来，我们不需要复杂而庞大的继承链，只要选择组合不同的类的功能，就可以快速构造出所需的子类。
 print('-----------------------------------------\n')
 
 
 #定制类////////////////////////////////////////////
+#__str__-------------------------------------------
+class Student(object):
+	def __init__(self, name):
+		self.name = name
+print('Student(\'jack\') =', Student('jack'))
+#怎么才能打印得好看呢？只需要定义好__str__()方法，返回一个好看的字符串就可以了：
+class Student(object):
+	def __init__(self, name):
+		self.name = name
+	def __str__(self):
+		return 'Student object (name: %s)' % self.name
+print('Student(\'jack\') =', Student('jack'))
+#但是细心的朋友会发现直接敲变量不用print，打印出来的实例还是不好看：
+#这是因为直接显示变量调用的不是__str__()，而是__repr__()，两者的区别是__str__()返回用户看到的字符串，而__repr__()返回程序开发者看到的字符串
+#也就是说，__repr__()是为调试服务的。
+#解决办法是再定义一个__repr__()。但是通常__str__()和__repr__()代码都是一样的，所以，有个偷懒的写法：
+class Student(object):
+	def __init__(self, name):
+		self.name = name
+	def __str__(self):
+		return 'Student object (name: %s)' % self.name
+	__repr__ = __str__
+print('-----------------------------------------')
+
+#__iter__-----------------------------------------------
+#如果一个类想被用于for ... in循环，类似list或tuple那样，就必须实现一个__iter__()方法，该方法返回一个迭代对象
+#然后，Python的for循环就会不断调用该迭代对象的__next__()方法拿到循环的下一个值，直到遇到StopIteration错误时退出循环。
+#我们以斐波那契数列为例，写一个Fib类，可以作用于for循环：
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1 # 初始化两个计数器a，b
+    def __iter__(self):
+        return self # 实例本身就是迭代对象，故返回自己
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b # 计算下一个值
+        if self.a > 100000: # 退出循环的条件
+            raise StopIteration();
+        return self.a # 返回下一个值
+#现在，试试把Fib实例作用于for循环：
+for n in Fib():
+	print(n)
+print('-----------------------------------------')
+
+#__getitem__----------------------------------------
+#Fib实例虽然能作用于for循环，看起来和list有点像，但是，把它当成list来使用还是不行，比如，取第5个元素：
+# Fib()[5]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'Fib' object does not support indexing
+#要表现得像list那样按照下标取出元素，需要实现__getitem__()方法：
+class Fib(object):
+    def __getitem__(self, n):
+        a, b = 1, 1
+        for x in range(n):
+            a, b = b, a + b
+        return a
+#现在，就可以按下标访问数列的任意一项了：
+f = Fib()
+print(f[0])
+print(f[100])
+print('-----------------------------------------')
+
+#__getattr__---------------------------------------------
+#正常情况下，当我们调用类的方法或属性时，如果不存在，就会报错。比如定义Student类：
+class Student(object):
+    def __init__(self):
+        self.name = 'Michael'
+#调用name属性，没问题，但是，调用不存在的score属性，就有问题了：
+#要避免这个错误，除了可以加上一个score属性外，Python还有另一个机制，那就是写一个__getattr__()方法，动态返回一个属性。修改如下：
+class Student(object):
+    def __init__(self):
+        self.name = 'Michael'
+    def __getattr__(self, attr):
+        if attr=='score':
+            return 99
+#当调用不存在的属性时，比如score，Python解释器会试图调用__getattr__(self, 'score')来尝试获得属性，这样，我们就有机会返回score的值：
+s = Student()
+print(s.name)
+print(s.score)
+#返回函数也是完全可以的：
+class Student(object):
+    def __getattr__(self, attr):
+        if attr=='age':
+            return lambda: 25
+#只是调用方式要变为：
+s = Student()
+print(s.age())
+#注意，只有在没有找到属性的情况下，才调用__getattr__，已有的属性，比如name，不会在__getattr__中查找。
+#此外，注意到任意调用如s.abc都会返回None，这是因为我们定义的__getattr__默认返回就是None。
+#要让class只响应特定的几个属性，我们就要按照约定，抛出AttributeError的错误：
+class Student(object):
+    def __getattr__(self, attr):
+        if attr=='age':
+            return lambda: 25
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+#这实际上可以把一个类的所有属性和方法调用全部动态化处理了，不需要任何特殊手段。
+print('-----------------------------------------')
+
+#__call__------------------------------------
+#一个对象实例可以有自己的属性和方法，当我们调用实例方法时，我们用instance.method()来调用。能不能直接在实例本身上调用呢？在Python中，答案是肯定的。
+#任何类，只需要定义一个__call__()方法，就可以直接对实例进行调用。请看示例：
+class Student(object):
+    def __init__(self, name):
+        self.name = name
+    def __call__(self):
+        print('My name is %s.' % self.name)
+#调用方式如下：
+s = Student('Jack')
+s()
+#__call__()还可以定义参数。对实例进行直接调用就好比对一个函数进行调用一样，所以你完全可以把对象看成函数，把函数看成对象，因为这两者之间本来就没啥根本的区别。
+#如果你把对象看成函数，那么函数本身其实也可以在运行期动态创建出来，因为类的实例都是运行期创建出来的，这么一来，我们就模糊了对象和函数的界限。
+#那么，怎么判断一个变量是对象还是函数呢？其实，更多的时候，我们需要判断一个对象是否能被调用，能被调用的对象就是一个Callable对象
+#通过callable()函数，我们就可以判断一个对象是否是“可调用”对象。
+print('-----------------------------------------\n')
+
+
+#使用枚举类////////////////////////////////////////
+#当我们需要定义常量时，一个办法是用大写变量通过整数来定义，例如月份：
+JAN = 1
+FEB = 2
+MAR = 3
+NOV = 11
+DEC = 12
+#好处是简单，缺点是类型是int，并且仍然是变量。
+#更好的方法是为这样的枚举类型定义一个class类型，然后，每个常量都是class的一个唯一实例。Python提供了Enum类来实现这个功能：
+from enum import Enum
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+#这样我们就获得了Month类型的枚举类，可以直接使用Month.Jan来引用一个常量，或者枚举它的所有成员：
+for name, member in Month.__members__.items():
+	print(name, '=>', member, ',', member.value)
+#value属性则是自动赋给成员的int常量，默认从1开始计数。
+#如果需要更精确地控制枚举类型，可以从Enum派生出自定义类：
+from enum import Enum,unique
+@unique
+class Weekday(Enum):
+    Sun = 0 # Sun的value被设定为0
+    Mon = 1
+    Tue = 2
+    Wed = 3
+    Thu = 4
+    Fri = 5
+    Sat = 6
+#@unique装饰器可以帮助我们检查保证没有重复值。
+#访问这些枚举类型可以有若干种方法：
+day1 = Weekday.Mon
+print(day1)
+print(Weekday.Tue)
+print(Weekday['Tue'])
+print(Weekday.Tue.value)
+print(day1 == Weekday.Mon)
+print(day1 == Weekday.Tue)
+print(Weekday(1))
+print(day1 == Weekday(1))
+for name, member in Weekday.__members__.items():
+    print(name, '=>', member)
+#可见，既可以用成员名称引用枚举常量，又可以直接根据value的值获得枚举常量。
+print('-----------------------------------------\n')
+
+
+#使用元类//////////////////////////////////////////////
+#type()--------------------------------------
+#动态语言和静态语言最大的不同，就是函数和类的定义，不是编译时定义的，而是运行时动态创建的。
+#比方说我们要定义一个Hello的class，就写一个hello.py模块：
+class Hello(object):
+    def hello(self, name='world'):
+        print('Hello, %s.' % name)
+#当Python解释器载入hello模块时，就会依次执行该模块的所有语句，执行结果就是动态创建出一个Hello的class对象，测试如下：
+h = Hello()
+print('h.hello() =', h.hello())
+print('type(Hello) =', type(Hello))
+print('type(h) =', type(h))
+#type()函数可以查看一个类型或变量的类型，Hello是一个class，它的类型就是type，而h是一个实例，它的类型就是class Hello。
+#我们说class的定义是运行时动态创建的，而创建class的方法就是使用type()函数。
+#type()函数既可以返回一个对象的类型，又可以创建出新的类型，比如，我们可以通过type()函数创建出Hello类，而无需通过class Hello(object)...的定义：
+def fn(self, name='world'):
+	print('Hello, %s.' % name)
+Hello = type('Hello', (object,), dict(hello=fn))   # 创建Hello class
+h = Hello()
+print('h.hello() =', h.hello())
+print('type(Hello) =', type(Hello))
+print('type(h) =', type(h))
+#要创建一个class对象，type()函数依次传入3个参数：
+#1.class的名称；
+#2.继承的父类集合，注意Python支持多重继承，如果只有一个父类，别忘了tuple的单元素写法；
+#3.class的方法名称与函数绑定，这里我们把函数fn绑定到方法名hello上。
+#通过type()函数创建的类和直接写class是完全一样的，因为Python解释器遇到class定义时，仅仅是扫描一下class定义的语法，然后调用type()函数创建出class。
+#正常情况下，我们都用class Xxx...来定义类，但是，type()函数也允许我们动态创建出类来，也就是说，动态语言本身支持运行期动态创建类，这和静态语言有非常大的不同
+#要在静态语言运行期创建类，必须构造源代码字符串再调用编译器，或者借助一些工具生成字节码实现，本质上都是动态编译，会非常复杂。
+print('-----------------------------------------')
+
+#metaclass------------------------------------------
+#这里回头再看看，没看太懂
